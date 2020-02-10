ba690fe7478c61a8674a2f87a64b9692f748998a
Zhenya <Canimore@yandex.ua>
2020-02-10 22:45:39 +0300

    refactoring 25%

--- Lab1/Lab.py
+++ Lab1/Lab.py
@@ -8,90 +8,22 @@ import itertools
 
 def Ex1():
     try:
-        money = float(input("Ввод: "))
-        if money < 0:
-            raise Exception("Ввод отрицательного числа")
-        rub = money//1
-        print(int(rub),"рублей")
-        print(round((money-rub)*100),"копеек")
-    except Exception as e:
-        print("ERROR ", e)
+        [print(int(i//1),"рублей",round((i-i//1)*100),"копеек") if i>= 0 else print(0/0) for i in [float(input("Ввод: "))]]
+    except ZeroDivisionError:
+        print("Некоректный формат")
 
 def Ex2():
-    array1 = [1,4,4,6]
-    array2 = list(range(10))
-    print(array1,"Первый\n",array2,"второй")
-    Ex2_1(array1)
-    Ex2_1(array2)
+    mass = [i for i in input("Введите набор чисел через пробел\n").split(" ")]
+    print(False not in [mass[i] < mass[i+1] for i in range(len(mass)-1)])
     
-def Ex2_1(x):
-    flag = True
-    past = x[0]
-    for i in x:
-        if past<i:
-            past = i
-        elif past==i and flag:
-            flag = False
-        elif past>=i and not flag:
-            print("False")
-            flag = True
-            break
-    if not flag:
-        print("True")
-
 def Ex3():
-    goodnum = "1222489822223131"
-    badnum = "123221321321311"
-    try:
-        snumber = input("Ввод: ")
-        if snumber == basegood:
-            snumber = goodnum
-            print(snumber)
-        elif snumber == basebad:
-            snumber = badnum
-            print(snumber)
-        snumber = snumber.replace(" ","")
-        number = int(snumber)
-        if len(str(number)) != 16:
-            s = "Вы ввели ",len(str(number)),"цифр вместо 16"
-            raise Exception(s)
-        count = 1
-        s = ""
-        for i in str(number):
-            if count == 5 or count ==9 or count == 13:
-                s=s+" "
-            if count <= 4 or count>=13:
-                s = s+ i
-            else:
-                s = s+"*"
-            count+=1
-        print(s)
-    except Exception as e:
-        print("ERROR ", e)
-        print("Возможные слова: ",basebad,basegood)    
+    snumber = input("Ввод: ").replace(" ","")
+    print(snumber.replace(snumber[4:12],'*'*8)) if len(snumber) == 16 else print("Неверно введенный номер")
 
 def Ex4():
-    stroka = "seseasdqd sadasd sadas s adqwdqwdwdqw asddqqd as qqqq wqesad reqwes"
-    arr = stroka.split(" ")
-    array = []
-    flag = True
-    for i in arr:
-        array.append(len(i))
-    array.sort(reverse=True)
-    for i in range(12,0,-1):
-        if flag and i>7:
-            print("Больше 7")
-            flag = False
-        elif i>=4 and i<=7 and not flag:
-            print("Меньше 7 и больше 4")
-            flag =True
-        elif i<4 and flag:
-            print("Меньше 4")
-            flag = False
-        for j in arr:
-            if i == len(j):
-                print(j)
-
+    arr = input("Введите текст\n").split(" ")
+    [print(i) for i in[print(i) if(i != None and len(i)<7 and len(i)>3 ) else i for i in[print(i) if len(i)>6 else i for i in arr]]if i!=None and len(i)<4]
+    
 def Ex5():
     ABC = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
     baseStroka = "город Донецк ,река Кальмиус"
